063a7f51606b3455c16a5c411f44676dbbcc2d6e
lootabox <tupelibox@gmail.com>
2020-01-06 20:04:55 +0200

    Stone Sentinels will not awaken after Raven is dead.

--- data/Scripts/CONTENT/Story/Events/B_Awake_StoneGuardian.d
+++ data/Scripts/CONTENT/Story/Events/B_Awake_StoneGuardian.d
@@ -2,7 +2,7 @@
 
 func VOID B_AWAKE_STONEGUARDIAN (var C_NPC slf)
 {	
-	if (slf.aivar[AIV_EnemyOverride] == TRUE)
+	if (RavenIsDead == FALSE) && (slf.aivar[AIV_EnemyOverride] == TRUE)
 	{	
 		Wld_PlayEffect("spellFX_INCOVATION_WHITE",  slf, slf, 0, 0, 0, FALSE );
 		Wld_PlayEffect("spellFX_LIGHTSTAR_RingRitual",  slf, slf, 0, 0, 0, FALSE );
