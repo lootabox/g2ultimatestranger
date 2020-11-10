2046cd65b43ebde123dc3ed8d40615d8d1b8b509
lootabox <tupelibox@gmail.com>
2020-11-10 17:58:30 +0200

    Update Text.d

--- data/Scripts/CONTENT/Story/Text.d
+++ data/Scripts/CONTENT/Story/Text.d
@@ -840,8 +840,6 @@ const string NAME_ADDON_PRAYIDOL_GIVEMANA				= "I will pray and offer 1 mana.";
 //CUSTOM
 // MAX LENGTH							"------------------------------------------------------------";
 const string NAME_Reach					= "Reach:";
-const string NAME_Rapier				= "Alternative Fighter and Master stances.";
-const string NAME_Rapier_Master			= "Alternative Fighter, Master and a true master stance.";
 
 // *************************************************************
 // NAMEN FÜR Produktionsitems
