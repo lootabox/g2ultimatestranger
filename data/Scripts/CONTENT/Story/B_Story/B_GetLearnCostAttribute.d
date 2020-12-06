069d23f95c4703f16c53f2268773e5dd64c3cf4b
lootabox <tupelibox@gmail.com>
2020-12-06 15:27:55 +0200

    Changed learning costs for attributes so they take into account extra points near cost increase thresholds

--- data/Scripts/CONTENT/Story/B_Story/B_GetLearnCostAttribute.d
+++ data/Scripts/CONTENT/Story/B_Story/B_GetLearnCostAttribute.d
@@ -40,3 +40,25 @@ func int B_GetLearnCostAttribute (var C_NPC oth, var int attribut)
 	
 	return kosten;
 };
+
+func int B_GetLearnCostAttribute2 (var C_NPC oth, var int attribute, var int amount)
+{
+	// Get current attribute value, without bonuses
+	var int current;
+		 if (attribute == ATR_STRENGTH)		{ current = oth.aivar[REAL_STRENGTH]; }
+	else if (attribute == ATR_DEXTERITY)	{ current = oth.aivar[REAL_DEXTERITY]; }
+	else if (attribute == ATR_MANA_MAX)		{ current = oth.aivar[REAL_MANA_MAX]; };
+
+	// Different attributes have different intervals for LP cost increases
+	var int interval;				  interval = 20;
+	if (attribute == ATR_MANA_MAX)	{ interval = 30; };
+
+	// Base cost (x amount) based on current attribute value
+	var int cost; cost = amount + amount * (current/interval);
+
+	// Extra value may be added near interval threshold to prevent exploiting
+	var int extra; extra = (current - interval * (current/interval)) + (amount - interval);
+		 if (extra > 5) { cost += 5; }
+	else if (extra > 0) { cost += extra; };
+	return cost;
+};
