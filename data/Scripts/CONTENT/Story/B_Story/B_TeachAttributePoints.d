103c51ac1d7582f8171692b6278bed3b55b6eb2b
lootabox <tupelibox@gmail.com>
2020-12-06 15:25:22 +0200

    Fixed actual attribute training costs to base off the real trained value instead of boosted value

--- data/Scripts/CONTENT/Story/B_Story/B_TeachAttributePoints.d
+++ data/Scripts/CONTENT/Story/B_Story/B_TeachAttributePoints.d
@@ -26,9 +26,9 @@ func int B_TeachAttributePoints (var C_NPC slf, var C_NPC oth, var int attrib, v
 	
 	// ------ Lernen NICHT über teacherMax ------
 	var int realAttribute;
-	if 		(attrib == ATR_STRENGTH)	{	realAttribute = oth.attribute[ATR_STRENGTH];	}	// Umwandeln von const-Parameter in VAR für folgende If-Abfrage
-	else if (attrib == ATR_DEXTERITY)	{	realAttribute = oth.attribute[ATR_DEXTERITY];	}
-	else if (attrib == ATR_MANA_MAX)	{	realAttribute = oth.attribute[ATR_MANA_MAX];	};
+	if 		(attrib == ATR_STRENGTH)	{	realAttribute = oth.aivar[REAL_STRENGTH];	}	// Umwandeln von const-Parameter in VAR für folgende If-Abfrage
+	else if (attrib == ATR_DEXTERITY)	{	realAttribute = oth.aivar[REAL_DEXTERITY];	}
+	else if (attrib == ATR_MANA_MAX)	{	realAttribute = oth.aivar[REAL_MANA_MAX];	};
 	
 	if (realAttribute >= teacherMAX)				//Wenn der Spieler schon das teacherMAX erreicht oder überschritten hat
 	{
