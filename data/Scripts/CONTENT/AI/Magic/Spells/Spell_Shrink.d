87836cc4ba085293eb9a7007810946600486d59c
lootabox <tupelibox@gmail.com>
2021-02-14 05:32:14 +0200

    Update Spell_Shrink.d

--- data/Scripts/CONTENT/AI/Magic/Spells/Spell_Shrink.d
+++ data/Scripts/CONTENT/AI/Magic/Spells/Spell_Shrink.d
@@ -3,9 +3,9 @@
 // **********
 
 const int SPL_Cost_Shrink			= 100;
-const int SPL_Shrink_Penalty		= 20; // 30%
-const int SPL_Shrink_Size			= 25; // 30%
-const int SPL_Shrink_Duration		= 3; // 3s
+const int SPL_Shrink_Penalty		= 20; // %
+const int SPL_Shrink_Size			= 25; // %
+const int SPL_Shrink_Duration		= 3; // seconds
 
 INSTANCE Spell_Shrink (C_Spell_Proto)
 {
