5702a11ce08d251acaa0c42a094f62149f82ebcf
lootabox <tupelibox@gmail.com>
2021-05-22 20:10:12 +0300

    Ignaz' dialog does not include lines without voice acting or create illogical log entry

--- data/Scripts/CONTENT/Story/Dialoge/DIA_VLK_498_Ignaz.d
+++ data/Scripts/CONTENT/Story/Dialoge/DIA_VLK_498_Ignaz.d
@@ -131,12 +131,12 @@ FUNC INT DIA_Ignaz_Experiment_Condition()
 FUNC VOID DIA_Ignaz_Experiment_Info()
 {
 	AI_Output (other, self, "DIA_Ignaz_Experiment_15_00");//Tell me more about the experiment and the spell.
-	AI_Output (self, other, "DIA_Ignaz_Experiment_14_01");//The spell serves to make somebody disremember various events.
+	AI_Output (self, other, "DIA_Ignaz_Experiment_14_01");//The spell serves to make somebody forget various events.
 	AI_Output (self, other, "DIA_Ignaz_Experiment_14_02");//So far, I have only found it to work when the person in question is angry - for instance, if he has just been knocked down, or robbed.
 	AI_Output (self, other, "DIA_Ignaz_Experiment_14_03");//Even if he has only witnessed such a deed, he will scratch it from his memory.
 	AI_Output (other, self, "DIA_Ignaz_Experiment_15_04");//So I'm supposed to knock somebody down and then put a spell on him?
-	AI_Output (other, self, "DIA_Ignaz_Add_15_00"); //That would only make sense if that person were REALLY mad after beating him up.
-	AI_Output (other, self, "DIA_Ignaz_Add_15_01"); //(to himself) But fistfights are nothing out of the ordinary in the harbor district. I guess I'd have to find a victim elsewhere...
+//	AI_Output (other, self, "DIA_Ignaz_Add_15_00"); //That would only make sense if that person were REALLY mad after beating him up.
+//	AI_Output (other, self, "DIA_Ignaz_Add_15_01"); //(to himself) But fistfights are nothing out of the ordinary in the harbor district. I guess I'd have to find a victim elsewhere...
 	AI_Output (self, other, "DIA_Ignaz_Experiment_14_05");//Yes, I think you get the point. But to make somebody angry, it is enough to attack him - you don't have to knock him down.
 	AI_Output (self, other, "DIA_Ignaz_Experiment_14_06");//So you should pick somebody who's by himself - if there are other people around, you'll just get into trouble with Lord Andre.
 	AI_Output (self, other, "DIA_Ignaz_Experiment_14_07");//Also, it makes no sense to cast the spell on someone who's busy attacking you. Wait for the right moment.
@@ -172,8 +172,8 @@ FUNC VOID DIA_Ignaz_teilnehmen_Info()
 	
 	Log_CreateTopic (TOPIC_Ignaz,LOG_MISSION);
 	Log_SetTopicStatus (TOPIC_Ignaz,LOG_RUNNING);
-	B_LogEntry (TOPIC_Ignaz,"I'm to test a new spell for Ignaz. It's an Oblivion spell. When anyone gets angry about a fight or for some other reason, he won't want to speak to me any more. This would be an ideal point to cast this spell.");
-	B_LogEntry (TOPIC_Ignaz,"This is the ideal opportunity for testing that spell. Then he'll forget he reported the matter too. I don't think anyone in the harbor district will get cross if I knock him down.");
+	B_LogEntry (TOPIC_Ignaz,"I'm to test a new spell for Ignaz. It's an Oblivion spell. When anyone gets angry about a fight or for some other reason, he won't want to speak to me any more. This would be an ideal point to cast this spell. I don't think anyone in the harbor district will get cross if I knock him down.");
+//	B_LogEntry (TOPIC_Ignaz,"This is the ideal opportunity for testing that spell. Then he'll forget he reported the matter too. I don't think anyone in the harbor district will get cross if I knock him down.");
 	
 	AI_StopProcessInfos (self);
 };
